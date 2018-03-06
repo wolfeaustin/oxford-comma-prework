7459228537d24d444e04668498c6712fda67cb46
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-06 04:19:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   sentence = ""
   return array[0] if array.size == 1 
+  return "#{array[0]} and #{array[1]}" if array.size == 2
   array.each_with_index do |el, i|
     if i == array.size - 1
       sentence << "and #{el}"
